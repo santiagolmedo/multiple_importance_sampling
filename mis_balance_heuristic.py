6c1e2cb2675ed5c7cf63dcf244de9742e5edfe65
santiagolmedo <santiolmedo99@gmail.com>
2023-09-10 22:56:51 -0300

    add different weights

--- mis_balance_heuristic.py
+++ mis_balance_heuristic.py
@@ -49,6 +49,33 @@ def compute_balance_heuristic_weights(X, ni, mu, sigma):
     ]
 
 
+def compute_power_heuristic_weights(X, ni, mu, sigma, beta=2):
+    """Compute the weights using the power heuristic."""
+    K = len(mu)
+    pi_X_values = [compute_p_k(X, mu[k], sigma[k]) for k in range(K)]
+    return [
+        (ni[i] * pi_X_values[i]) ** beta
+        / sum([(ni[k] * pi_X_values[k]) ** beta for k in range(K)])
+        for i in range(K)
+    ]
+
+
+def compute_cutoff_heuristic_weights(X, ni, mu, sigma):
+    """Compute the weights using the cutoff heuristic."""
+    K = len(mu)
+    pi_X_values = [compute_p_k(X, mu[k], sigma[k]) for k in range(K)]
+    max_val_index = np.argmax([ni[k] * pi_X_values[k] for k in range(K)])
+    return [1 if i == max_val_index else 0 for i in range(K)]
+
+
+def compute_maximum_heuristic_weights(X, ni, mu, sigma):
+    """Compute the weights using the maximum heuristic."""
+    K = len(mu)
+    pi_X_values = [compute_p_k(X, mu[k], sigma[k]) for k in range(K)]
+    max_val = max([ni[k] * pi_X_values[k] for k in range(K)])
+    return [(ni[i] * pi_X_values[i]) / max_val for i in range(K)]
+
+
 def compute_mis_estimate(N, alfai, mu, sigma, a, b):
     """Compute the MIS estimate."""
     K = len(alfai)
@@ -64,7 +91,7 @@ def compute_mis_estimate(N, alfai, mu, sigma, a, b):
             Y = compute_function_values(X, a, b)
 
             # Compute the weights
-            weights = compute_balance_heuristic_weights(X, ni, mu, sigma)
+            weights = compute_power_heuristic_weights(X, ni, mu, sigma)
 
             # Add the sampled point values to the lists
             sampled_points_X.append(X)
@@ -134,7 +161,7 @@ def main():
     b = mu + 2 * sigma
 
     # Seed for reproducibility
-    np.random.seed(8)
+    # np.random.seed(8)
 
     # Compute MIS estimate
     Imis, sampled_points_X, _ = compute_mis_estimate(N, alfai, mu, sigma, a, b)
