ffcc2abd9f30cd3606ef08fc4d4cbc853256baee
santiagolmedo <santiolmedo99@gmail.com>
2023-10-22 17:09:08 -0300

    --wip-- [skip ci]

--- mis_general2plot.py
+++ mis_general2plot.py
@@ -1,5 +1,6 @@
 import numpy as np
 import mpmath
+import matplotlib.pyplot as plt
 
 def calculate_function_values(x, a, b, m, n):
     """Calculate the values of the function to be integrated."""
@@ -81,10 +82,80 @@ def calculate_mis_estimate(total_samples, a, b, m, n):
 
     return estimate, sampled_points_x, sampled_points_y, variance, alternate_variance
 
+# Function for 2D visualization
+
+def plot_2d_results(a, b, m, n):
+    x = np.linspace(-2, 2, 100)
+    y = np.linspace(-2, 2, 100)
+
+    X, Y = np.meshgrid(x, y)
+    Z_func = np.zeros_like(X)
+    Z_pdf = np.zeros_like(X)
+
+    for i in range(X.shape[0]):
+        for j in range(X.shape[1]):
+            input_val = np.array([[X[i, j], Y[i, j]] for _ in range(m)])
+            Z_func[i, j] = calculate_function_values(input_val, a, b, m, n)
+            Z_pdf[i, j] = calculate_pdf_i(input_val, a, b, m, n, 0)  # Using first distribution as an example
+
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5))
+
+    # Plotting the function
+    c1 = ax1.contourf(X, Y, Z_func, cmap='viridis')
+    ax1.set_title('Function to be Integrated')
+    fig.colorbar(c1, ax=ax1)
+
+    # Plotting the PDF
+    c2 = ax2.contourf(X, Y, Z_pdf, cmap='viridis')
+    ax2.set_title('Probability Density Function (PDF)')
+    fig.colorbar(c2, ax=ax2)
+
+    plt.tight_layout()
+    plt.show()
+
+# Function for 3D side view visualization
+
+def plot_3d_side_view(a, b, m, n):
+    x = np.linspace(-2, 2, 100)
+    y = np.linspace(-2, 2, 100)
+
+    X, Y = np.meshgrid(x, y)
+    Z_func = np.zeros_like(X)
+    Z_pdf = np.zeros_like(X)
+
+    for i in range(X.shape[0]):
+        for j in range(X.shape[1]):
+            input_val = np.array([[X[i, j], Y[i, j]] for _ in range(m)])
+            Z_func[i, j] = calculate_function_values(input_val, a, b, m, n)
+            Z_pdf[i, j] = calculate_pdf_i(input_val, a, b, m, n, 0)  # Using first distribution as an example
+
+    fig = plt.figure(figsize=(14, 6))
+
+    # Plotting the function in 3D
+    ax1 = fig.add_subplot(1, 2, 1, projection='3d')
+    ax1.plot_surface(X, Y, Z_func, cmap='viridis')
+    ax1.view_init(elev=0, azim=-90)  # side view
+    ax1.set_title('Function to be Integrated')
+    ax1.set_xlabel('X')
+    ax1.set_ylabel('Y')
+    ax1.set_zlabel('Z')
+
+    # Plotting the PDF in 3D
+    ax2 = fig.add_subplot(1, 2, 2, projection='3d')
+    ax2.plot_surface(X, Y, Z_pdf, cmap='viridis')
+    ax2.view_init(elev=0, azim=-90)  # side view
+    ax2.set_title('Probability Density Function (PDF)')
+    ax2.set_xlabel('X')
+    ax2.set_ylabel('Y')
+    ax2.set_zlabel('Z')
+
+    plt.tight_layout()
+    plt.show()
+
 def main():
     """Main function to compute MIS estimate and plot the results."""
     NUM_SAMPLES = 50
-    a = np.array([[1.0, 1.0], [1.0, 1.0]])
+    a = np.array([[2.0, 2.0], [2.0, 2.0]])
     b = np.array([[0.0, 0.0], [0.0, 0.0]])
     m = 2
     n = 2
@@ -97,6 +168,9 @@ def main():
     print(f"Variance of the integral with MIS: {variance}")
     print(f"Alternate variance of the integral with MIS: {alternate_variance}")
     print(f"Exact result of the integral: {calculate_exact_integral(a, m, n)}")
+
+    plot_2d_results(a, b, m, n)
+    plot_3d_side_view(a, b, m, n)
 
 if __name__ == "__main__":
     main()
